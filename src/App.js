95ab25f9dba8526239055c86c5de4f54357a6a38
liana.valeria15 <liana.valeria15@gmail.com>
2022-02-05 11:02:31 +0000

    import and export component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+// step 1 - declare your component as a function
+const App = () => {
+  //react components always have to return JSX (html)
+  return <h1>Hello from React</h1>;
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+//step 2 - export your component
 export default App;
